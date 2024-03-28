a7df15a625a151edb64502570bee1e3a60284d59
Shewanji <marrionwanjiru26@gmail.com>
2024-03-28 02:27:27 +0000

    add namespace to urls

--- myApp/urls.py
+++ myApp/urls.py
@@ -2,11 +2,16 @@ from django.urls import path
 
 from . import views
 
+app_name = 'myApp'
+
 urlpatterns = [
         path("", views.home, name="home"),
-        path('employee/create/', views.create_employee, name='create_employee'),
-        path('employee/list/', views.employee_list, name='employee_list'),
-        path('employee/<int:pk>/', views.employee_detail, name='employee_detail'),
-        path('employee/<int:pk>/update/', views.update_employee, name='update_employee'),
-        path('employee/<int:pk>/delete/', views.delete_employee, name='delete_employee'),
+        path('login/', views.user_login, name='user_login'),
+        path('signup/', views.signup, name='signup'),
+        path('logout/', views.user_logout, name='user_logout'),
+        path('create/', views.create_employee, name='create_employee'),
+        path('list/', views.employee_list, name='employee_list'),
+        path('<int:pk>/', views.employee_detail, name='employee_detail'),
+        path('<int:pk>/update/', views.update_employee, name='update_employee'),
+        path('<int:pk>/delete/', views.delete_employee, name='delete_employee'),
 ]
--- myProject/urls.py
+++ myProject/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myApp.urls')),
+    path('login/employee/', include('myApp.urls', namespace='employee_management')),
 ]
 
